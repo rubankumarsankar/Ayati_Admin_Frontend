898ce5f30e5e468de09b41312c074998e5bfa937
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2025-09-18 19:01:42 +0530

    18/09/2025 Update 1

--- src/api/services.js
+++ src/api/services.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const Contactform = async (formData) => {
   try {
-    const response = await axios.post("https://ayatiworks.com/Api/contact-uslead.php?name=ruban&mobile=8111040614&email=rubankumar@ayatiworks.com&budget=Below%20$1000&message=Hii&services=Social%20Media%20Marketing,SEO%20and%20Content%20Marketing,Influencer%20Marketing,Website%20%26%20Landing%20Pages", formData);
+    const response = await axios.post("https://ayatiworks.com/Api/contactlead.php", formData);
     return response.data;
   } catch (error) {
     console.error("Contactform API Error:", error);
